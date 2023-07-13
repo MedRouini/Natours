d141ae8f09133c1654d65b93cd8f9edbee12e9fc
MedRouini <medrouini@outlook.com>
2023-07-13 21:27:03 +0200

    added createBookingCheckout function in teh bookingController.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 const multer = require('multer');
+const bookingModel = require('../models/bookingModel');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
@@ -11,7 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -35,4 +36,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const {tour, user, price} = req.query;
+
+  if (!tour && !user && !price) return next();
+  await bookingModel.create({tour, user, price});
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isUserLoggedIn, viewsController.getOverview);
+router.get('/', 
+  bookingController.createBookingCheckout, 
+  authController.isUserLoggedIn, 
+  viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isUserLoggedIn, viewsController.getTour);
 router.get('/login', authController.isUserLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protectRoute, viewsController.getAccount);
